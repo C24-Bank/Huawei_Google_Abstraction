401cb199ba2993a29b3fce814f05c63fa4dcd2df
Hoang Ha Vu <hoang.ha@check24.de>
2021-08-16 12:05:37 +0200

    made all the functions for the notificationService abstract

--- google-cloudmessaging/src/main/java/de/c24/hg_abstraction/NotificationService.kt
+++ google-cloudmessaging/src/main/java/de/c24/hg_abstraction/NotificationService.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.lang.Exception
 
 abstract class NotificationService: FirebaseMessagingService() {
 
@@ -19,80 +20,14 @@ abstract class NotificationService: FirebaseMessagingService() {
 
     private lateinit var NOTIFICATION_ID: String
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
+    abstract override fun onMessageReceived(remoteMessage: RemoteMessage)
 
-        Log.e("message","Message Received ...");
+    abstract override fun onNewToken(p0: String)
 
-        if (remoteMessage.data.isNotEmpty()) {
-            val title = remoteMessage.data["title"]
-            val body = remoteMessage.data["body"]
-            showNotification(applicationContext, title, body)
-        } else {
-            val title = remoteMessage.notification!!.title
-            val body = remoteMessage.notification!!.body
-            showNotification(applicationContext, title, body)
-        }
-    }
+    abstract override fun onDeletedMessages()
 
-    override fun onNewToken(p0: String) {
-        super.onNewToken(p0)
-        Log.e("token","New Token")
-    }
+    abstract override fun onMessageSent(p0: String)
 
-    fun showNotification(
-        context: Context,
-        title: String?,
-        message: String?
-    ) {
-        val ii: Intent = Intent(context, activity::class.java)
-        ii.data = Uri.parse("custom://" + System.currentTimeMillis())
-        ii.action = "actionstring" + System.currentTimeMillis()
-        ii.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
-        val pi =
-            PendingIntent.getActivity(context, 0, ii, PendingIntent.FLAG_UPDATE_CURRENT)
-        val notification: Notification
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            //Log.e("Notification", "Created in up to orio OS device");
-            notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                .setColor(getNotificationColor())
-                .setOngoing(true)
-                .setSmallIcon(getNotificationIcon())
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setContentIntent(pi)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(Notification.CATEGORY_SERVICE)
-                .setWhen(System.currentTimeMillis())
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentTitle(title).build()
-            val notificationManager = context.getSystemService(
-                Context.NOTIFICATION_SERVICE
-            ) as NotificationManager
-            val notificationChannel = NotificationChannel(
-                NOTIFICATION_CHANNEL_ID,
-                title,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(notificationChannel)
-            notificationManager.notify(NOTIFICATION_ID.toInt(), notification)
-        } else {
-            notification = NotificationCompat.Builder(context)
-                .setSmallIcon(getNotificationIcon())
-                .setAutoCancel(true)
-                .setContentText(message)
-                .setContentIntent(pi)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentTitle(title).build()
-            val notificationManager = context.getSystemService(
-                Context.NOTIFICATION_SERVICE
-            ) as NotificationManager
-            notificationManager.notify(NOTIFICATION_ID.toInt(), notification)
-        }
-    }
-
-    abstract fun getNotificationIcon(): Int
-
-    abstract fun getNotificationColor(): Int
-
+    abstract override fun onSendError(p0: String, p1: Exception)
+    
 }
--- huawei-pushkit/src/main/java/de/c24/hg_abstraction/NotificationService.kt
+++ huawei-pushkit/src/main/java/de/c24/hg_abstraction/NotificationService.kt
@@ -9,7 +9,7 @@ import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.RemoteMessage
 import com.huawei.hms.push.SendException
 
-class NotificationService: HmsMessageService() {
+abstract class NotificationService: HmsMessageService() {
 
     private lateinit var activity: Activity
 
@@ -17,93 +17,18 @@ class NotificationService: HmsMessageService() {
 
     private lateinit var NOTIFICATION_ID: String
 
-    val TAG = "PushDemoLog"
-    val CODELABS_ACTION= "com.huawei.codelabpush.action"
+    abstract override fun onNewToken(token: String?, bundle: Bundle?)
 
-    override fun onNewToken(token: String?, bundle: Bundle?) {
-        super.onNewToken(token);
-        // Obtain a token.
-        Log.i(TAG, "have received refresh token:$token")
+    abstract override fun onMessageReceived(message: RemoteMessage?)
 
-        // Check whether the token is empty.
-        if (!token.isNullOrEmpty()) {
-            refreshedTokenToServer(token)
-        }
-    }
+    abstract override fun onMessageSent(msgId: String?)
 
-    private fun refreshedTokenToServer(token: String) {
-        Log.i(TAG, "sending token to server. token:$token")
-    }
+    abstract override fun onSendError(msgId: String?, exception: Exception?)
 
-    override fun onMessageReceived(message: RemoteMessage?) {
-        Log.i(TAG, "onMessageReceived is called")
+    abstract override fun onTokenError(e: Exception)
 
-        // Check whether the message is empty.
-        if (message == null)
-        {
-            Log.e(TAG, "Received message entity is null!")
-            return
-        }
+    abstract override fun onMessageDelivered(msgId: String?, exception: Exception?)
 
-        // Obtain the message content.
-        Log.i(TAG, """getData: ${message.data}        
-            getFrom: ${message.from}        
-            getTo: ${message.to}        
-            getMessageId: ${message.messageId}
-            getSendTime: ${message.sentTime}           
-            getDataMap: ${message.dataOfMap}
-            getMessageType: ${message.messageType}   
-            getTtl: ${message.ttl}        
-            getToken: ${message.token}""".trimIndent())
-
-        val judgeWhetherIn10s = false
-        // If the message is not processed within 10 seconds, create a job to process it.
-        if (judgeWhetherIn10s) {
-            startWorkManagerJob(message)
-        } else {
-            // Process the message within 10 seconds.
-            processWithin10s(message)
-        }
-    }
-    private fun startWorkManagerJob(message: RemoteMessage?) {
-        Log.d(TAG, "Start new Job processing.")
-    }
-    private fun processWithin10s(message: RemoteMessage?) {
-        Log.d(TAG, "Processing now.")
-    }
-
-
-    override fun onMessageSent(msgId: String?) {
-        Log.i(TAG, "onMessageSent called, Message id:$msgId")
-        val intent = Intent()
-        intent.action = CODELABS_ACTION
-        intent.putExtra("method", "onMessageSent")
-        intent.putExtra("msg", "onMessageSent called, Message id:$msgId")
-        sendBroadcast(intent)
-    }
-
-    override fun onSendError(msgId: String?, exception: Exception?) {
-        Log.i(TAG, "onSendError called, message id:$msgId, ErrCode:${(exception as SendException).errorCode}, " +
-                "description:${exception.message}")
-        val intent = Intent()
-        intent.action = CODELABS_ACTION
-        intent.putExtra("method", "onSendError")
-        intent.putExtra("msg", "onSendError called, message id:$msgId, ErrCode:${exception.errorCode}, " +
-                "description:${exception.message}")
-        sendBroadcast(intent)
-    }
-
-    override fun onTokenError(e: Exception) {
-        super.onTokenError(e)
-    }
-
-    override fun onMessageDelivered(msgId: String?, exception: Exception?) {
-        // Obtain the error code and description.
-        val errCode = (exception as BaseException).errorCode
-        val errInfo = exception.message
-        if (errCode != 0) {
-            // Process specific service requirements.
-        }
-    }
+    abstract override fun onDeletedMessages()
 
 }

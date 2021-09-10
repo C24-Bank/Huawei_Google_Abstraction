273771e8e071cdfc07537b729c64bbe70d9a5acb
Hoang Ha Vu <hoang.ha@check24.de>
2021-09-10 11:22:31 +0200

    remodeled startcamera method for scanview and scanactivity

--- huawei-scankit/src/main/java/de/c24/hg_abstraction/ScanActivity.kt
+++ huawei-scankit/src/main/java/de/c24/hg_abstraction/ScanActivity.kt
@@ -37,35 +37,12 @@ class ScanActivity : AppCompatActivity() {
     }
 
     private fun startCamera(){
-        //1.get screen density to caculate viewfinder's rect
-        val dm = resources.displayMetrics
-        //2.get screen size
-        val density = dm.density
-        mScreenWidth=dm.widthPixels
-        mScreenHeight=dm.heightPixels
-        var scanFrameSize=(SCAN_FRAME_SIZE*density)
-        //3.caculate viewfinder's rect,it's in the middle of the layout
-        //set scanning area(Optional, rect can be null,If not configure,default is in the center of layout)
-        val rect = Rect()
-        apply {
-            rect.left = (mScreenWidth / 2 - scanFrameSize / 2).toInt()
-            rect.right = (mScreenWidth / 2 + scanFrameSize / 2).toInt()
-            rect.top = (mScreenHeight / 2 - scanFrameSize / 2).toInt()
-            rect.bottom = (mScreenHeight / 2 + scanFrameSize / 2).toInt()
-        }
+        //caculate viewfinder's rect,it's in the middle of the layout
+        val rect = calculateRect()
+
         //initialize RemoteView instance, and set calling back for scanning result
-        remoteView = RemoteView.Builder().setContext(this).setBoundingBox(rect).setFormat(HmsScan.ALL_SCAN_TYPE).build()
-        remoteView?.onCreate(null)
-        remoteView?.setOnResultCallback { result ->
-            if (result != null && result.size > 0 && result[0] != null && !TextUtils.isEmpty(result[0].getOriginalValue())) {
-                val hmsScanResult: HmsScan = result[0]
-                val intent = Intent()
-                intent.apply {
-                    putExtra(SCAN_RESULT, hmsScanResult.getOriginalValue()) }
-                setResult(Activity.RESULT_OK, intent)
-                this.finish()
-            }
-        }
+        remoteView = initializeRemoteView(this,rect)
+
         // Add the defined RemoteView to the page layout.
         val params = FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
         val frameLayout = findViewById<FrameLayout>(R.id.rim1)

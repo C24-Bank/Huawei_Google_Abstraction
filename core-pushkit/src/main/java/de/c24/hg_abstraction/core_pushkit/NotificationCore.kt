7570dac3566060038cfe98917d69446275f18c6a
Hoang Ha Vu <hoang.ha@check24.de>
2021-09-28 15:25:10 +0200

    added changes to pushkit core and added the classes NotificationRemoteMessage and NotificationCore

---
+++ core-pushkit/src/main/java/de/c24/hg_abstraction/core_pushkit/NotificationCore.kt
@@ -0,0 +1,36 @@
+package de.c24.hg_abstraction.core_pushkit
+
+import android.net.Uri
+
+data class NotificationCore(
+    val titleLockKey: String,
+    val title: String,
+    val ticker: String,
+    val tag: String,
+    val sound: String,
+    val huaweiIntentUri: String? = null,
+    val huaweiNotifiyId: Int? = null,
+    val huaweiImageUrl: Uri? = null,
+    val googleImageUrl: String? = null,
+    val link: Uri,
+    val icon: String,
+    val color: String,
+    val clickAction: String,
+    val channelId: String,
+    val notificationPriority:Int,
+    val bodyLocalizationKey: String,
+    val body: String,
+    val visibility: Int,
+    val notificationCount: Int,
+    val eventTime: Long,
+    val defaultVibrate: Boolean,
+    val defaultSound: Boolean,
+    val defaultLight: Boolean,
+    val sticky: Boolean,
+
+    val googleVibrateTiming: LongArray? = null,
+    val lightSettings: IntArray,
+    val titleLockArgs: Array<String>,
+    val bodyLockArgs: Array<String>,
+    val huaweiVibrateConfig: LongArray
+)
